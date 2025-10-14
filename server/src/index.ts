68e18fd26eab802805c71867af2b43dbdf9ac0df
Manish <[manishbulchandani4@gmail.com]>
2025-10-14 22:33:04 +0530

    server deploy test

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
+
 app.use(cors({
     origin: [
         process.env.CLIENT_URL || 'http://localhost:3000',
